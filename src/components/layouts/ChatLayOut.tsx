a636ffad2e5439aa889513852707298f5067a125
Guidotss <guidoolguin@hotmail.com>
2023-03-28 17:50:46 -0300

    initial commit

---
+++ src/components/layouts/ChatLayOut.tsx
@@ -0,0 +1,35 @@
+
+import { FC } from "react"
+import Head from "next/head"
+import { SideBar } from "../index"; 
+
+interface ChatLayOutProps {
+    children: React.ReactNode; 
+    title?: string; 
+    description?: string;
+    image?: string; 
+}
+
+export const ChatLayOut:FC<ChatLayOutProps> = ({ title, description, image, children }) => {
+  return (
+    <>
+        <Head>
+            <title>{ title || "Chat App" } </title>
+            <meta name="author" content="Guido Olguin" />
+            <meta name="keywords" content="Chat, App, Next.js" />
+            <meta name="robots" content="index, follow" />
+            <meta name="description" content={description || "Chat App"} />
+            <meta name="og:title" content={title || "Chat App" }/>
+            <meta name="og:description" content={ description || "Chat App" }/>
+            {
+                image && <meta name="og:image" content={ image } />
+            }
+        </Head>
+        
+        <main className="w-full h-screen bg-gptgray relative">
+            <SideBar/> 
+            { children }
+        </main>
+    </>
+  )
+}
---
+++ src/components/ui/SideBar.tsx
@@ -0,0 +1,8 @@
+export const SideBar = () => {
+  return (
+    <div className="bg-gptdarkgray fixed flex flex-col w-[260px] h-screen text-slate-200 overflow-auto sidebar-scroll-bar">
+  
+    </div>
+    
+  )
+}
