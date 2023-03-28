20332f3575af3b392b2ebd37a51d84342d31773d
Guidotss <guidoolguin@hotmail.com>
2023-03-28 18:15:41 -0300

    Agregando mocks de datos sidebar

--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image"
+
 export const SideBar = () => {
   return (
     <div className="bg-gptdarkgray fixed flex flex-col w-[260px] h-screen text-slate-200 overflow-auto sidebar-scroll-bar">
-  
+      <div className="flex border-[1px] p-2 rounded-md w-[240px] self-center mt-5">
+        <Image
+          src="/plus.webp"
+          alt="Picture of the author"
+          width={30}
+          height={10}
+        />
+        <div className="ml-5 self-center">
+          <span>Nuevo Chat</span>
+        </div>
+      </div>
     </div>
     
   )
---
+++ src/data/mockSidebar.ts
@@ -0,0 +1,19 @@
+export const sidebarData = [
+    {
+        id: 1,
+        description:'no se puede crear.',
+    },
+    {
+        id: 2,
+        description:'Saludo y pregunta.',
+    },
+    {
+        id: 3,
+        description:'Ajedrez con coordenadas.',
+    },
+    {
+        id: 4,
+        description:'Redaccion de carta legal.',
+    }
+
+] as const; 
